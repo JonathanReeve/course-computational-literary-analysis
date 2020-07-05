ea5654e0a59eda58588ec089f6db9fb9990173fb
Jonathan Reeve <jon.reeve@gmail.com>
2020-07-05 19:10:21 -0400

    remove nbgrader stuff

--- Homework/nbgrader-setup.sh
+++
@@ -1,5 +0,0 @@
-# Instructions adapted from https://nbgrader.readthedocs.io/en/latest/user_guide/installation.html 
-pipenv install nbgrader jupyterlab
-jupyter nbextension install --sys-prefix --py nbgrader --overwrite
-jupyter nbextension enable --sys-prefix --py nbgrader
-jupyter serverextension enable --sys-prefix --py nbgrader
